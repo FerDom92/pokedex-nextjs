24600187cf2821ca2e359dde47603716964db202
Fernando Dominguez <fernando.dom1992@gmail.com>
2024-08-25 09:01:37 -0300

    changed name of files

--- src/hooks/usePokemonDetails.ts
+++ src/hooks/usePokemonDetails.ts
@@ -1,9 +1,9 @@
+import { pokeApiClient } from '@/lib/apiClient';
 import { useQuery } from '@tanstack/react-query';
-import PokeApiClient from '../services/PokeApi';
 
 export const usePokemonDetails = (id: number | string) => {
   return useQuery({
     queryKey: ['pokemonDetail', id],
-    queryFn: () => PokeApiClient.getInstance().getPokemonDetails(id),
+    queryFn: () => pokeApiClient.getPokemonDetails(id),
   });
 };
